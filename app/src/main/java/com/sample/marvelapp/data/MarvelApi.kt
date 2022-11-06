195b09c5c608928638cba713614a67391dcfb7ce
Ömer Yıldız <yildizomer94@gmail.com>
2022-11-06 20:29:06 +0300

    Add AppModule, API interface

---
+++ app/src/main/java/com/sample/marvelapp/data/MarvelApi.kt
@@ -0,0 +1,4 @@
+package com.sample.marvelapp.data
+
+interface MarvelApi {
+}
---
+++ app/src/main/java/com/sample/marvelapp/di/AppModule.kt
@@ -0,0 +1,49 @@
+package com.sample.marvelapp.di
+
+import com.sample.marvelapp.data.MarvelApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().also {
+            it.level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        // todo: add baseUrl
+        return Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): MarvelApi {
+        return retrofit.create(MarvelApi::class.java)
+    }
+}
