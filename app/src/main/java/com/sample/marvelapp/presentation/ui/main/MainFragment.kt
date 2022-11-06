b58dee500e2ba3180dea9700b035a29924ff1b30
Ömer Yıldız <yildizomer94@gmail.com>
2022-11-06 22:23:33 +0300

    Add MainFragment

---
+++ app/src/main/java/com/sample/marvelapp/presentation/ui/main/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.sample.marvelapp.presentation.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.sample.marvelapp.R
+import com.sample.marvelapp.databinding.FragmentMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment: Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
